af255a8f80b81b1832ec70b5fb20fced3ee94ce1
smbarajas <32750643+smbarajas@users.noreply.github.com>
2018-05-02 21:22:09 -0700

    Add files via upload

---
+++ web120/contact.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="css/form.css" />
+<?php include "includes/header.php"?>
+<h2>Contact Sarah</h2>
+<?php include "includes/simple.php";?>
+<p class="clear-recaptcha"></p>
+<?php include "includes/footer.php"?>
---
+++ web120/index.php
@@ -0,0 +1,16 @@
+<?php include "includes/header.php"?>
+<!-- END Header include here! -->
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <p>My name is Sarah Barajas, and this is my second year here at Seattle Central. I started off pursuing a degree in social services but had a change of heart and now I'm currently pursuing my Associates Degree in Information Systems Software Development. Once I have graduated I am hoping to transfer to Seattle University to get my bachelor's. My dream is to work as a programmer for a large tech firm such as Microsoft, Amazon, or Expedia.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <img src="images/Whatever.jpg" class="selfie" alt="" />
+ <h3>Right Column</h3>
+ <p>In my spare time I enjoy playing computer games, wandering around bookstores and reading, watching horror movies, taking things apart to learn about how they work, reading through r/nosleep on reddit and listening to music. I also love black clothing, looking up the camera specs of upcoming smartphones, bath products, and trying new foods! </p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php"?>
